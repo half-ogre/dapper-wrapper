378d64548f06f8cac1207d3a1aec35a51b53c81d
Edgar Ivaskevicius <eivaskevicius@navinet.net>
2012-11-13 17:14:35 -0500

    Extended IDBExecutor interface.
    Added more methods available in Dapper's SqlMapper to IDBExecutor
    interface and implementation.

--- DapperWrapper/SqlExecutor.cs
+++ DapperWrapper/SqlExecutor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using Dapper;
@@ -61,6 +62,107 @@ namespace DapperWrapper
                 buffered,
                 commandTimeout,
                 commandType);
+        }
+
+        public IEnumerable<TReturn> Query<T1, T2, TReturn>(
+            string sql,
+            Func<T1, T2, TReturn> map,
+            object param = null,
+            IDbTransaction transaction = null,
+            bool buffered = true,
+            string splitOn = "Id",
+            int? commandTimeout = default(int?),
+            CommandType? commandType = default(CommandType?))
+        {
+            return _sqlConnection.Query(
+                sql,
+                map,
+                param,
+                transaction,
+                buffered,
+                splitOn,
+                commandTimeout,
+                commandType);
+        }
+
+        public IEnumerable<TReturn> Query<T1, T2, T3, TReturn>(
+            string sql,
+            Func<T1, T2, T3, TReturn> map,
+            object param = null,
+            IDbTransaction transaction = null,
+            bool buffered = true,
+            string splitOn = "Id",
+            int? commandTimeout = default(int?),
+            CommandType? commandType = default(CommandType?))
+        {
+            return _sqlConnection.Query(
+                sql,
+                map,
+                param,
+                transaction,
+                buffered,
+                splitOn,
+                commandTimeout,
+                commandType);
+        }
+
+        public IEnumerable<TReturn> Query<T1, T2, T3, T4, TReturn>(
+            string sql,
+            Func<T1, T2, T3, T4, TReturn> map,
+            object param = null,
+            IDbTransaction transaction = null,
+            bool buffered = true,
+            string splitOn = "Id",
+            int? commandTimeout = default(int?),
+            CommandType? commandType = default(CommandType?))
+        {
+            return _sqlConnection.Query(
+                sql,
+                map,
+                param,
+                transaction,
+                buffered,
+                splitOn,
+                commandTimeout,
+                commandType);
+        }
+
+        public IEnumerable<TReturn> Query<T1, T2, T3, T4, T5, TReturn>(
+            string sql,
+            Func<T1, T2, T3, T4, T5, TReturn> map,
+            object param = null,
+            IDbTransaction transaction = null,
+            bool buffered = true,
+            string splitOn = "Id",
+            int? commandTimeout = default(int?),
+            CommandType? commandType = default(CommandType?))
+        {
+            return _sqlConnection.Query(
+                sql,
+                map,
+                param,
+                transaction,
+                buffered,
+                splitOn,
+                commandTimeout,
+                commandType);
+        }
+
+        public IGridReaderWrapper QueryMultiple(
+            string sql,
+            object param = null,
+            IDbTransaction transaction = null,
+            int? commandTimeout = null,
+            CommandType? commandType = null)
+        {
+            var gridReader = _sqlConnection.QueryMultiple(
+                    sql,
+                    param,
+                    transaction,
+                    commandTimeout,
+                    commandType);
+
+            return new GridReaderWrapper(gridReader);
         }
 
         public void Dispose()
